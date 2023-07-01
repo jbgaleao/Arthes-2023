9760ac2d56565f30a4259b690d0d2c53392e0c80
Joao Galeao <jbgaleao@gmail.com>
2023-06-30 23:28:17 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Arthes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Arthes.MvcApplication" Language="C#" %>
